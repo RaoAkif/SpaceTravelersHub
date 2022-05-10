07ba069b4bec3473a2a1477078b3a99cf3696a96
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-05-10 21:54:40 +0500

    Add the Navbar, and Link it with Pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import logo from '../assets/images/logo.png';
 
-export const Header = () => {
+const navBar = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+};
+
+function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container style={navBar}>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src={logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />
+            {' '}
+            Space Traverlers Hub
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Link className="navLink" to="/">Rockets</Link>
+            <Link className="navLink" to="/missions">Missions</Link>
+            <Link className="navLink" to="/myprofile">My Profile</Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
 }
+
+export default Header;
