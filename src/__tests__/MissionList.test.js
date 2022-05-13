da528b8183474ab108e679535a5a5af5bd84714f
raoakif <61361037+RaoAkif@users.noreply.github.com>
2022-05-13 20:31:11 +0500

    Write tests for Header, Rockets, MissionList

--- src/__tests__/MissionList.test.js
+++ src/__tests__/MissionList.test.js
@@ -2,13 +2,13 @@ import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/configureStore';
-import Table from '../pages/Missions/MissionTable';
+import MissionList from '../pages/Missions/MissionList';
 
 it('renders correctly', () => {
   const tree = renderer
     .create(
       <Provider store={store}>
-        <Table />
+        <MissionList />
       </Provider>,
     )
     .toJSON();
--- src/__tests__/Rockets.test.js
+++ src/__tests__/Rockets.test.js
@@ -1,14 +1,14 @@
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import store from '../../redux/configureStore';
-import MissionList from '../../pages/Missions/MissionList';
+import store from '../redux/configureStore';
+import Rocket from '../components/Rocket/Rocket';
 
 it('renders correctly', () => {
   const tree = renderer
     .create(
       <Provider store={store}>
-        <MissionList />
+        <Rocket />
       </Provider>,
     )
     .toJSON();
